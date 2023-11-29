09d501efc979f0f6bb4b66ce12351ac6333fe2a5
webemp <bhargavigreeweb@gmail.com>
2023-11-29 12:26:54 +0530

    fourth-commit

--- src/components/Buttons/GoogleDrivePick.jsx
+++ src/components/Buttons/GoogleDrivePick.jsx
@@ -1,51 +1,71 @@
 import React, { useState, useEffect } from 'react';
 
+const DISCOVERY_DOCS = ['https://www.googleapis.com/discovery/v1/apis/drive/v3/rest'];
+const SCOPES = 'https://www.googleapis.com/auth/drive.metadata.readonly';
+const CLIENT_ID = '497857861442-obkjgko2u2olskde533rvf6i21f2khd3.apps.googleusercontent.com';
+const API_KEY = 'AIzaSyDRBMb3f8y_DY4_TCpJeo3vO5ctJsd7YHg';
+
 const GoogleDrivePick = () => {
   const [files, setFiles] = useState([]);
 
-  useEffect(() => {
-    // Load the Google API Client
-    const script = document.createElement('script');
-    script.src = 'https://apis.google.com/js/api.js';
-    script.onload = () => { };
-    document.body.appendChild(script);
-  }, []);
+  const authenticate = () => {
+    window.gapi.load('client:auth2', () => {
+      window.gapi.client.init({
+        apiKey: API_KEY,
+        clientId: CLIENT_ID,
+        discoveryDocs: DISCOVERY_DOCS,
+        scope: SCOPES,
+      }).then(() => {
+        const authInstance = window.gapi.auth2.getAuthInstance();
 
-  const initializePicker = () => {
-    window.gapi.load('picker', () => {
-      // Create a Picker instance
-      const picker = new window.google.picker.PickerBuilder()
-        .addViewGroup(
-          new window.google.picker.ViewGroup(window.google.picker.ViewId.DOCS)
-            .addView(window.google.picker.ViewId.DOCUMENTS)
-        )
-        .setOAuthToken(window.gapi.auth.getToken().access_token)
-        .setDeveloperKey('AIzaSyDRBMb3f8y_DY4_TCpJeo3vO5ctJsd7YHg')
-        .setCallback(pickerCallback)
-        .build();
-
-      // Show the Picker
-      picker.setVisible(true);
+        // Check if the user is already signed in
+        if (!authInstance.isSignedIn.get()) {
+          // If not, show the sign-in popup
+          authInstance.signIn().then(() => {
+            listFiles();
+          });
+        } else {
+          // If already signed in, proceed to list files
+          listFiles();
+        }
+      });
     });
   };
 
-  const pickerCallback = (data) => {
-    if (data.action === window.google.picker.Action.PICKED) {
-      const selectedFile = data.docs[0];
-      console.log('Selected file:', selectedFile);
-      // Perform actions with the selected file, e.g., download, display details, etc.
+  const listFiles = async () => {
+    try {
+      const response = await window.gapi.client.drive.files.list({
+        pageSize: 10,
+        fields: 'files(id, name)',
+      });
+      const files = response.result.files;
+      setFiles(files);
+    } catch (error) {
+      console.error('Error fetching files:', error);
     }
   };
 
-  const handleImportClick = () => {
-    // Trigger the file picker when the "Import from Google Drive" button is clicked
-    initializePicker();
+  useEffect(() => {
+    authenticate();
+  }, []);
+
+  const handleFileSelect = (fileId) => {
+    // Perform actions with the selected file, e.g., download, display details, etc.
+    console.log(`Selected file with ID: ${fileId}`);
   };
 
   return (
     <div>
-
-      <button onClick={handleImportClick}>Import from Google Drive</button>
+      <h1>Google Drive Files</h1>
+      <button onClick={authenticate}>Sign In</button>
+      <ul>
+        {files.map((file) => (
+          <li key={file.id}>
+            {file.name}{' '}
+            <button onClick={() => handleFileSelect(file.id)}>Select</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
